e7b763f1af436815c828007790a96b8b5ff98f2f
Laszlo Borsos <laszlo.borsos1@hu.ibm.com>
2021-12-13 10:49:46 +0100

    Last commit path to the delete session does not work

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,9 @@
 class Stock < ApplicationRecord
+    def self.new_lookup(ticker_symbol)
+        client = IEX::Api::Client.new( publishable_token: 'publishable_token',
+            secret_token: 'secret_token',
+            endpoint: 'https://cloud.iexapis.com/v1'
+          )
+
+    end
 end
